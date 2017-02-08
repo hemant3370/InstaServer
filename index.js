7cbba5bd08d9aa6acc46b680fb814cc971864f72
hemant3370 <hemant@hatchitup.com>
2017-02-08 18:48:15 +0530

    chages

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.get('/json/:string', routes.me);
 app.get('*', function(req, res) {
-		res.sendFile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-	});
+	res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
 
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
